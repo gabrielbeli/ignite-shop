18f6c8a68541ba2844299003ea811d61d3d3d2f4
gabriel <gabrielimabelo@gmail.com>
2024-05-29 20:52:37 +0100

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,27 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
+import Shirt1 from '../assets/Shirt-1.png'
+import Shirt2 from '../assets/Shirt-2.png'
+import Shirt3 from '../assets/Shirt-3.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8 )'
-  }
-})
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={Shirt1} width={520} height={480} alt={""} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={Shirt2} width={520} height={480} alt={""} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
 )
 }
