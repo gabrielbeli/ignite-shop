6224ac79b981688af94bdf64cdd1d5e290225853
gabriel <gabrielimabelo@gmail.com>
2024-05-30 23:25:07 +0100

    Checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,10 +11,14 @@ interface ProductProps {
     imageUrl: string,
     price: string,
     description: string,
+    defaultPriceId: string,
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId);
+  }
 
   return (
     <ProductContainer>
@@ -26,7 +30,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
 
         <p>{product.description}</p>
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -61,6 +65,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
         currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1,
